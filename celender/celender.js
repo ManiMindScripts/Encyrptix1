c8cd5874831de2ef5cb986a9be34abda291e2399
ManiMindScripts <ar7279796@gmail.com>
2024-07-04 12:56:16 -0700

    Cmp our Task

---
+++ celender/celender.js
@@ -0,0 +1,92 @@
+const header = document.querySelector(".celender h3");
+const dates = document.querySelector(".dates");
+const prevbtn = document.querySelector("#prev");
+const nextbtn = document.querySelector("#next");
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]; 
+
+let date =new Date();
+let month = date.getMonth();
+let year = date.getFullYear();
+
+function rendercelender(){
+    const start = new Date(year, month, 1).getDay();
+    const endDate = new Date(year,month + 1, 0).getDate();
+    const end = new Date(year, month ,endDate).getDay();
+    const endDatePrev = new Date(year,month , 0).getDate();
+
+    let datesHtml = "";
+
+    for(let i = start; i > 0; i-- )
+        {
+            datesHtml += `<li class="inactive"> ${endDatePrev - i + 1 }</li> `;
+        }
+        for(let i = 1; i <= endDate; i++) {
+            let className =
+            i === date.getDate()&&
+            month === new Date().getMonth()&&
+            year === new Date().getFullYear()
+            ? '"class=today"'
+            :"";
+            datesHtml += `<li ${className}>${i}</li>`;
+        }
+        for(let i = end + 1; i <=6; i++) {
+            datesHtml += `<li class="inactive"> ${i - end  }</li> `;
+        }
+        dates.innerHTML  = datesHtml;
+        header.textContent = ` ${months[month]} ${year}`;
+
+    }
+    prevbtn.addEventListener("click", () => {
+        if (month === 0) {
+            year--;
+            month = 11;
+        } else {
+            month--;
+        }
+        rendercelender();
+    });
+    nextbtn.addEventListener("click", () => {
+        if (month === 11) {
+            year++;
+            month = 0;
+        } else {
+            month++;
+        }
+        rendercelender();
+    });
+//     navs.forEach((nav) => {
+//         nav.addEventListener("click",(e) => {
+//             const btnId  = e.target.id;
+               
+//             if(btnId === "prev" &&  month === 0 ){
+//              year--;
+//              month = 11;
+//             }else if(btnId === "next" && month === 11 ){
+//                 year++;
+//                 month = 0;
+//             }else {
+//                 month  = btnId === "next" ? month + 1 : month - 1;
+//             }
+//             date = new Date(year, month, new Date().getDate());
+//             year = date.getFullYear();
+//             month = date.getMonth();
+
+// rendercelender();
+
+//         });
+//     });
+rendercelender();
